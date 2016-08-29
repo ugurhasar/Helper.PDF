1402a3f0ad1a75a73e1530587e42028918cb4036
Uğur Hasar <ugurhasar@outlook.com>
2016-08-30 00:17:23 +0300

    new version

--- Quick.Assistant.Pdf.Test/Program.cs
+++ Quick.Assistant.Pdf.Test/Program.cs
@@ -1,11 +1,6 @@
-﻿using iTextSharp.text;
-using iTextSharp.text.pdf;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Quick.Assistant.Pdf.Test
 {
@@ -13,65 +8,21 @@ namespace Quick.Assistant.Pdf.Test
     {
         static void Main(string[] args)
         {
-//            var assembly = Assembly.GetExecutingAssembly();
-//var resourceName = "MyCompany.MyProduct.MyFile.txt";
+            string currentLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
-//using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            string html = File.ReadAllText(string.Concat(currentLocation, "\\template\\index.html"));
+            string style = File.ReadAllText(string.Concat(currentLocation, "/template/style.css"));
 
-            PdfProcess pdfProcess = new PdfProcess();
-            Stream stream = File.Open(@"D:\score_card-1.pdf", FileMode.Open);
+            byte[] bytes = PDFHelper.Write(html, style, Encoding.UTF8, 20, 20, 20, 20);
 
-            MemoryStream memoryStream = new MemoryStream();
+            string fileName = string.Concat(currentLocation, "\\pdf_test.pdf");
 
-            PdfReader pdfReader = new PdfReader(stream);
-
-            PdfStamper pdfStamper = new PdfStamper(pdfReader, memoryStream);
-
-            AcroFields pdfFormFields = pdfStamper.AcroFields;
-
-            Rectangle rec = pdfProcess.GetFieldPostions(pdfStamper, "txtReference");
-
-            PdfPTable table = new PdfPTable(5);
-            table.WidthPercentage = 100;
-
-            PdfPCell cell00_0 = pdfProcess.CreateCell("#", 0, 5, 8, 5, true);
-            cell00_0.BorderWidthRight = 0.5f;
-            table.AddCell(cell00_0);
-            PdfPCell cell0_0 = pdfProcess.CreateCell("Kat", 0, 5, 8, 10, true);
-            table.AddCell(cell0_0);
-            PdfPCell cell1_0 = pdfProcess.CreateCell("Kat m²", 0, 5, 8, 5, true);
-            table.AddCell(cell1_0);
-            PdfPCell cell2_0 = pdfProcess.CreateCell("Kiriş Altı Yükseklik", 0, 5, 8, 5, true);
-            table.AddCell(cell2_0);
-            PdfPCell cell3_0 = pdfProcess.CreateCell("Tavan Yüksekliği", 0, 5, 8, 5, true);
-            table.AddCell(cell3_0);
-
-            Random random = new Random();
-
-            for (int i = 1; i < 100; i++)
+            if (File.Exists(fileName))
             {
-                PdfPCell cell00_1 = pdfProcess.CreateCell(i.ToString(), 10, 5, 8, 5);
-                cell00_1.BorderWidthRight = 0.5f;
-                table.AddCell(cell00_1);
-                PdfPCell cell0_1 = pdfProcess.CreateCell((i - 10).ToString(), 10, 5, 8, 10);
-                table.AddCell(cell0_1);
-                PdfPCell cell1_1 = pdfProcess.CreateCell(random.Next(80, 200).ToString(), 10, 5, 8, 5);
-                table.AddCell(cell1_1);
-                PdfPCell cell2_1 = pdfProcess.CreateCell(random.Next(2, 10).ToString(), 10, 5, 8, 5);
-                table.AddCell(cell2_1);
-                PdfPCell cell3_1 = pdfProcess.CreateCell(random.Next(2, 10).ToString(), 10, 5, 8, 5);
-                table.AddCell(cell3_1);
+                File.Delete(fileName);
             }
 
-            table.SetTotalWidth(new float[] { 30, 95, 95, 150, 150 });
-
-            pdfProcess.AddTableExistingDocument(table, pdfReader, pdfStamper, 3, rec);
-            pdfProcess.AddPageNumaber(pdfReader, pdfStamper);
-            pdfProcess.AddHeader(pdfReader, pdfStamper, "Score Card (SCA-1)");
-
-            pdfStamper.Close();
-
-            File.WriteAllBytes("D:\\itextsharp.pdf", memoryStream.ToArray());
+            File.WriteAllBytes(fileName, bytes);
         }
     }
 }
---
+++ Quick.Assistant.Pdf/PDFHelper.cs
@@ -0,0 +1,56 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.tool.xml.html;
+using iTextSharp.tool.xml.parser;
+using iTextSharp.tool.xml.pipeline.css;
+using iTextSharp.tool.xml.pipeline.end;
+using iTextSharp.tool.xml.pipeline.html;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Quick.Assistant.Pdf
+{
+    public static class PDFHelper
+    {
+        public static byte[] Write(string template, string style, float marginLeft = 0, float marginRight = 0, float marginTop = 0, float marginBottom = 0)
+        {
+            return Write(template, style, Encoding.UTF8, marginLeft, marginRight, marginTop, marginBottom);
+        }
+
+        public static byte[] Write(string template, string style, Encoding encoding, float marginLeft = 0, float marginRight = 0, float marginTop = 0, float marginBottom = 0)
+        {
+            byte[] bytes = null;
+
+            if (string.IsNullOrWhiteSpace(template))
+            {
+                throw new Exception("template is null.");
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (Document document = new Document(PageSize.A4, marginLeft, marginRight, marginTop, marginBottom))
+                {
+                    using (var pdfWriter = PdfWriter.GetInstance(document, memoryStream))
+                    {
+                        document.Open();
+
+                        using (var css = new MemoryStream(encoding.GetBytes(style)))
+                        {
+                            using (var html = new MemoryStream(encoding.GetBytes(template)))
+                            {
+                                XMLWorkerHelper.GetInstance().ParseXHtml(pdfWriter, document, html, css);
+                            }
+                        }
+                        document.Close();
+
+                    }
+                }
+                bytes = memoryStream.ToArray();
+            }
+
+            return bytes;
+        }
+    }
+}
